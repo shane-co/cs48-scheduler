2f1bca6d9d23acf5c39fa766771f1364a44bdd49
jleeong <jared.leeong13@gmail.com>
2017-01-30 13:34:11 -0800

    Update TimeBlock.java

--- src/client/app/TimeBlock.java
+++ src/client/app/TimeBlock.java
@@ -6,23 +6,40 @@ import client.app.Day;
 /**
 *Class representing a TimeBlock within a schedule. Is the basic unit within a schedule and holds only information regarding the
 *block of time, its availability, and a reference to the ScheduleEvent that is occupying it. Takes 24hr time notation to denote 
-*start and end of the time block that it represents
+*start of the time block that it represents. Duration of a TimeBlock is 1hr
 */
 public class TimeBlock{
 
     private Day;
     private String start;
-    private String end;
     private ScheduleEvent se;
     private boolean occupied;
   
     /**
     *Default Constructor
+    *@param d the Day event that this TimeBlock belongs to.
+    *@param s the time that the TimeBlock starts
+    *@param ev the ScheduleEvent that occupies this TimeBlock. NULL if no event associated
+    *@param occ boolean indicating availability. Must be True if ev!=NULL
     */
-    public TimeBlock(Day d, String s, String e, ScheduleEvent ev, boolean occ){}
+    public TimeBlock(Day d, String s, ScheduleEvent ev, boolean occ){}
   
+    /**
+    *Function to retrieve time information regarding the TimeBlock
+    *@return String of format "yyyy-mm-dd hh:mm" in 24hr time notation
+    */
     public String getBlock(){}
+    
+    /**
+    *Function to retrieve ScheduleEvent that is associated with TimeBlock
+    *@return ScheduleEvent object
+    */
     public ScheduleEvent getEvent(){}
+    
+    /**
+    *Function to determine availability of TimeBlock
+    *@return TRUE if se!=NULL; FALSE otherwise
+    */
     public boolean isOccupied(){}
   
     /**
