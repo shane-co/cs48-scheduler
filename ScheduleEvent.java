4afb00d7afe667a09c131519fd0b1c9580c665d8
Tianyi6679 <timtianyima@gmail.com>
2017-02-15 21:49:29 -0800

    Add files via upload

--- ScheduleEvent.java
+++ ScheduleEvent.java
@@ -11,18 +11,34 @@ import client.app.obj.TimeBlock;
 public class ScheduleEvent{
 
     private ArrayList<Dependencies> deps;
+    private int day;
     private int start;
     private int end;
     private String description;
-    public ScheduleEvent(ArrayList<Dependencies> d,int s, int e, String dp)
+    public ScheduleEvent(ArrayList<Dependencies> dep,int d,int s, int e, String dp)
     {
-    	deps=d;
+    	deps=dep;
+    	day=d;
     	start=s;
     	end=e;
     	description=dp;
     }
     
+    public ScheduleEvent(int d,int s, int e, String dp)
+    {
+    	day=d;
+    	start=s;
+    	end=e;
+    	description=dp;
+    }
+    
+    public int what_day() {return day;}
     public int when_to_start() {return start; }
     public int when_to_end() {return end; }
+    public int duration() {return start-end;}
     public String get_descpt() {return description; }
+    public void set_day(int d){day=d;}
+    public void set_start(int s) {start=s; }
+    public void set_to_end(int e) { end=e; }
+    public void set_descpt(String dp) { description=dp; }
 }
