9a3c202b8b02904e4ae17c646fe2ee3a41775376
Chucheng Lin <398549982@qq.com>
2017-02-16 13:09:52 -0800

    Update User.java

--- src/client/app/obj/User.java
+++ src/client/app/obj/User.java
@@ -4,6 +4,8 @@ package client.app.obj;
 import client.app.obj.ScheduleEvent;
 import client.app.obj.Schedule;
 import client.app.obj.Database;
+import java.util.Iterator;
+import java.util.ArrayList;
 
 /**
 *Class representing a user using the Application. Maintains all data about the user and their
@@ -12,12 +14,76 @@ import client.app.obj.Database;
 */
 public class User{
     //instance variables
-    private ArrayList<Schedule> mySchedules;
-    private ArrayList<ScheduleEvent> myHostedEvents;
-    private ArrayList<ScheduleEvent> myEvents;
-    private ArrayList<DatabaseConnection> myOrgs;
-
+    private ArrayList<Schedule> mySchedules = new ArrayList<Schedule>();
+    private ArrayList<ScheduleEvent> myHostedEvents = new ArrayList<ScheduleEvent>();
+    private ArrayList<ScheduleEvent> myEvents = new ArrayList<ScheduleEvent>();
+    private ArrayList<DatabaseConnection> myOrgs = new ArrayList<DatabaseConnection>();
+    
+  //Functions to modify instance variables;
+    
+    public void addSchedule(Schedule s){
+    	this.mySchedules.add(s);
+    }
+    
+    public void addScheduleEvent(ScheduleEvent s){
+    	this.myHostedEvents.add(s);
+    }
+    
+    public void addScheduleEvent(ScheduleEvent s){
+    	this.myEvents.add(s);
+    }
+    
+    public void addDatabaseConnection(DatabaseConnection d){
+    	this.myOrgs.add(d);
+    }
+    
+    
     //Functions to retrieve from instance variables;
+    public void removemySchedule(Schedule s){
+    	Iterator<Schedule> iter = mySchedules.iterator();
+    	while (iter.hasNext()){
+    		Schedule ss = iter.next();
+    		if (s == ss)
+    		{
+    	    	this.mySchedules.remove(s);
 
-    //Functions to modify instsance variables;
+    		}
+    	}
+    }
+    
+    public void removeScheduleEvent(ScheduleEvent s){
+    	Iterator<ScheduleEvent> iter = myEvents.iterator();
+    	while (iter.hasNext()){
+    		ScheduleEvent ss = iter.next();
+    		if (s == ss)
+    		{
+    	    	this.myEvents.remove(s);
+
+    		}
+    	}
+    }
+    
+    public void removemyHostedEvents(ScheduleEvent s){
+    	Iterator<ScheduleEvent> iter = myHostedEvents.iterator();
+    	while (iter.hasNext()){
+    		ScheduleEvent ss = iter.next();
+    		if (s == ss)
+    		{
+    	    	this.myHostedEvents.remove(s);
+
+    		}
+    	}
+    }
+    
+    public void removeDatabaseConnection(DatabaseConnection d){
+    	Iterator<DatabaseConnection> iter = myOrgs.iterator();
+    	while (iter.hasNext()){
+    		DatabaseConnection ss = iter.next();
+    		if (d == ss)
+    		{
+    	    	this.myOrgs.remove(d);
+
+    		}
+    	}
+    }
 }
