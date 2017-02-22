708c2d3b22e53e00195cc3c58b68a8482ec1dee0
jasonorozco <orozcojason@yahoo.com>
2017-02-21 22:16:17 -0800

    just a test

--- src/client/commander/ScheduleGenerator.java
+++ src/client/commander/ScheduleGenerator.java
@@ -1,2 +1,37 @@
 package client.commander;
-public class ScheduleGenerator{}
+
+import 
+public class ScheduleGenerator{
+/**
+* The class generates a schedule 
+*/
+	private ArrayList<ScheduleEvent> theEvents;
+	private ArrayList<ScheduleEvent> theSortedEvents;
+	private ArrayList<ArrayList<ScheduleEvent>> listOLists = new ArrayList<ArrayList<ScheduleEvent>>();
+	private myEventsSize;
+	
+	public ScheduleGenerator(){
+		theEvents = client.getUserEvents();
+		myEventsSize = theEvents.size()
+	}
+	
+	private boolean collision(ScheduleEvent ev1, ScheduleEvent ev2){
+		if(ev1.whenToEnd() <= ev2.)
+			return false;
+		else
+			return true;
+	}
+	
+	
+	for (int j = 0; j < myEventsSize; j++) {
+		int shortestStart = -1;
+		int shortestIndex;
+		for(int i = 0; i<myEventsSize; i++){
+			if(theEvents.whenToStart() > shortestStart){
+				shortestStart = theEvents.whenToStart();
+			}
+		}
+	}
+	
+
+}
