37e4fcd2677e4721e90a8f2aae21afedc2c7ab53
jasonorozco <orozcojason@yahoo.com>
2017-02-21 22:57:55 -0800

    some more additions 2/12/2017

--- src/client/commander/ScheduleGenerator.java
+++ src/client/commander/ScheduleGenerator.java
@@ -8,15 +8,15 @@ public class ScheduleGenerator{
 	private ArrayList<ScheduleEvent> theEvents;
 	private ArrayList<ScheduleEvent> theSortedEvents;
 	private ArrayList<ArrayList<ScheduleEvent>> listOLists = new ArrayList<ArrayList<ScheduleEvent>>();
-	private myEventsSize;
+	private int myEventsSize;
 	
 	public ScheduleGenerator(){
 		theEvents = client.getUserEvents();
-		myEventsSize = theEvents.size()
+		myEventsSize = theEvents.size();
 	}
 	
 	private boolean collision(ScheduleEvent ev1, ScheduleEvent ev2){
-		if(ev1.whenToEnd() <= ev2.)
+		if(ev1.whenToEnd() <= ev2.whenToStart())
 			return false;
 		else
 			return true;
@@ -24,13 +24,19 @@ public class ScheduleGenerator{
 	
 	
 	for (int j = 0; j < myEventsSize; j++) {
-		int shortestStart = -1;
+		int earliestStart = 2500;
+		int oldEarliestStart = -1;
 		int shortestIndex;
-		for(int i = 0; i<myEventsSize; i++){
-			if(theEvents.whenToStart() > shortestStart){
-				shortestStart = theEvents.whenToStart();
+		for(int i = 0; i< myEventsSize; i++){
+			if(earliestStart > theEvents.get(i).whenToStart() && earliestStart > oldEarliestStart){
+				earliestStart = theEvents.whenToStart();
+				shortestIndex = i;
+			}
+			else{
+				
 			}
 		}
+		oldEarliestStart
 	}
 	
 
