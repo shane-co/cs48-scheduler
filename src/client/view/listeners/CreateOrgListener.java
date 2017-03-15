a33917d684a1745f300ea2fae73cf654c9dbb78b
shane-co <seamuss.mck@gmail.com>
2017-03-14 21:36:09 -0700

    Update CreateOrgListener.java

--- src/client/view/listeners/CreateOrgListener.java
+++ src/client/view/listeners/CreateOrgListener.java
@@ -5,14 +5,25 @@ import client.commander.BGCommander;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import javax.swing.JTable;
 
 public class CreateOrgListener implements ActionListener{
     private JTextField id,orgip,port;
-    public CreateOrgListener(JTextField i, JTextField ip, JTextField p){
-        id=i; orgip=ip; port=p;
+    private JTable table;
+    public CreateOrgListener(JTextField i, JTextField ip, JTable table){
+        id=i; orgip=ip; this.table = table;
     }
     public void actionPerformed(ActionEvent e){
-        BGCommander.getBGCommander().addOrganization(id.getText(),orgip.getText(),port.getText());
+    	//if(!existsInTable()){
+        BGCommander.getBGCommander().addOrganization(id.getText(),orgip.getText());
         UserInterface.getUserInterface().refreshDisplay();
+    	//}
     }
+    /*public boolean existsInTable(){
+    	int rowCount=table.getRowCount();
+    	for (int i=0; i<rowCount; i++){
+    		if (id.getText().equals((String)table.getValueAt(i,0).toString())) return true;
+    	}
+    	return false;
+    }*/
 }
