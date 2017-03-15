2618ff9ea28c36bc079c9cc4ee45186b6d7ad45e
shane-co <seamuss.mck@gmail.com>
2017-03-14 21:36:38 -0700

    Create OrgRemoveListener.java

---
+++ src/client/view/listeners/OrgRemoveListener.java
@@ -0,0 +1,28 @@
+package client.view.listeners;
+
+import client.view.UserInterface;
+import client.commander.BGCommander;
+import javax.swing.JTable;
+import javax.swing.DefaultListModel;
+import javax.swing.ListModel;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+public class OrgRemoveListener implements ActionListener{
+	JTable table;
+	public OrgRemoveListener(JTable table){
+		this.table = table;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		String orgToBeDeleted = table.getModel().getValueAt(table.getSelectedRow(),0).toString();
+		BGCommander.getBGCommander().deleteFromField(orgToBeDeleted , "org");
+		UserInterface.getUserInterface().refreshDisplay();
+	}
+}
